1d95c6580769ded9763ac4a5a1993bf6c4a4e985
boldfaced7 <boldfaced7@naver.com>
2025-01-10 12:12:01 +0900

    init

---
+++ src/test/java/com/mypay/MyPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
